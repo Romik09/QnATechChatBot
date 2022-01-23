e69eb6b090fd7278199b6a02bd9e389299ea365f
Romik09 <92760071+Romik09@users.noreply.github.com>
2022-01-23 12:02:57 +0530

    Add files via upload

---
+++ qnatechbot-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnatechbot-bot-bb57-Web-Deploy.pubxml -p:Password=s9sC0AyHv22RPRxe9fYxibsjEz93KbxTqa62uj3ypJxkxsHSjJrZlZa4jirL
+
